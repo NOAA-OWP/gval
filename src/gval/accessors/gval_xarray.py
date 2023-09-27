901f069b857ca42f26418a1627e553224b35c830
Gregory Petrochenkov <greg.petrochenkov@noaa.gov>
2023-09-27 11:31:59 -0400

    remove main statement

--- src/gval/accessors/gval_xarray.py
+++ src/gval/accessors/gval_xarray.py
@@ -823,26 +823,3 @@ class GVALXarray:
         """
 
         return _vectorize_data(self._obj)
-
-
-if __name__ == "__main__":
-    import rioxarray as rxr
-
-    path = "/home/sven/repos/gval/notebooks/"
-    subsample_df = gpd.read_file(f"{path}subsample_two-class_polygons.gpkg")
-    subsample_df.gval.create_subsampling_df(subsampling_type="include", inplace=True)
-
-    candidate = rxr.open_rasterio(
-        f"{path}candidate_map_multiband_two_class_categorical.tif", mask_and_scale=True
-    )
-    benchmark = rxr.open_rasterio(
-        f"{path}benchmark_map_multiband_two_class_categorical.tif", mask_and_scale=True
-    )
-
-    ag, ctab, met = candidate.gval.categorical_compare(
-        benchmark_map=benchmark,
-        positive_categories=[2],
-        negative_categories=[0, 1],
-        subsampling_df=subsample_df,
-        subsampling_average="full-detail",
-    )
